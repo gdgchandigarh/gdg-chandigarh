3199a42c9dbbbb8cb9d17efe00a8203b8484d685
Kamal Kumar <kamal.kumar@tixtrackcom>
2023-09-20 14:48:43 +0530

    Added teampages and basic modificiations

--- src/app/events/event-speaker/event-speaker.component.ts
+++ src/app/events/event-speaker/event-speaker.component.ts
@@ -1,57 +1,122 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-event-speaker',
   templateUrl: './event-speaker.component.html',
   styleUrls: ['./event-speaker.component.css']
 })
-export class EventSpeakerComponent {
-  speakers = [
+export class EventSpeakerComponent implements OnInit {
+ 
+speakers = [
     {
     id:1,
     image:"https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg",
-    name:"Demo User 1",
+    name:"A Demo User",
     company:"IBM",
     designation:"Team Lead",
     profile:"",
     twitter:"",
-  },
-  {
-    id:2,
-    image:"https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg",
-    name:"Demo Speaker 2",
-    company:"KPMG",
-    designation:"Developer",
-    profile:"",
-    twitter:"",
-  },
-  {
-    id:3,
-    image:"https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg",
-    name:"Demo Speaker 3",
-    company:"Astrotalk",
-    designation:"Software Engineer",
-    profile:"",
-    twitter:"",
+    linkedin: "",
+    instagram: "",
+    sessions: [
+      {
+        sessionId: 1,
+        sessionTopic: "Demo Session 1",
+        sessionDuration: "40 min"
+      }
+    ],
+    bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
   },
   {
     id:4,
     image:"https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg",
-    name:"Demo Speaker 4",
+    name:"D Demo Speaker",
     company:"Infosys Limited",
     designation:"Technology Lead",
     profile:"",
     twitter:"",
+    linkedin: "",
+    instagram: "",
+    sessions: [
+      {
+        sessionId: 1,
+        sessionTopic: "Demo Session 1",
+        sessionDuration: "40 min"
+      }
+    ],
+    bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+  },
+  {
+    id:2,
+    image:"https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg",
+    name:"B Demo Speaker",
+    company:"KPMG",
+    designation:"Developer",
+    profile:"",
+    twitter:"",
+    linkedin: "",
+    instagram: "",
+    sessions: [
+      {
+        sessionId: 1,
+        sessionTopic: "Demo Session 1",
+        sessionDuration: "40 min"
+      }
+    ],
+    bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+  },
+  {
+    id:3,
+    image:"https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg",
+    name:"C Demo Speaker",
+    company:"Astrotalk",
+    designation:"Software Engineer",
+    profile:"",
+    twitter:"",
+    linkedin: "",
+    instagram: "",
+    sessions: [
+      {
+        sessionId: 1,
+        sessionTopic: "Demo Session 1",
+        sessionDuration: "40 min"
+      }
+    ],
+    bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
   },
   {
     id:5,
     image:"https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg",
-    name:"Demo Speaker 5",
+    name:"E Demo Speaker",
     company:"Nagarro",
     designation:"Senior Engineer",
     profile:"",
     twitter:"",
-  },
+    linkedin: "",
+    instagram: "",
+    sessions: [
+      {
+        sessionId: 1,
+        sessionTopic: "Demo Session 1",
+        sessionDuration: "40 min"
+      }
+    ],
+    bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+  }
 ];
+
+
+ngOnInit(): void {
+  this.speakers.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    } else if (a.name > b.name) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+}
+
 
 }
